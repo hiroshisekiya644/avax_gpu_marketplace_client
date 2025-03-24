2b813f44d4d5f0b85bff709f8443a48e242fee09
hiroshisekiya644 <hiroshisekiya644@gmail.com>
2025-03-24 09:27:21 +0100

    fix:remove profile page

--- src/components/layouts/sidebar/Sidebar.tsx
+++ src/components/layouts/sidebar/Sidebar.tsx
@@ -40,7 +40,7 @@ export const sidebarClusterMenu: SidebarMenu[] = [
   { name: 'Reserved Instances', href: '/dashboard/quotes', icon: <QuoteIcon /> },
   { name: 'Intelligence', href: '/dashboard/intelligence', icon: <TrainingIcon /> },
   { name: 'Billing', href: '/dashboard/billing', icon: <BillingIcon /> },
-  { name: 'Profile', href: '/dashboard/profile', icon: <ProfileIcon /> }
+  // { name: 'Profile', href: '/dashboard/profile', icon: <ProfileIcon /> }
   // { name: 'Providers', href: '/dashboard/providers', icon: <ProvidersIcon /> },
   // { name: 'Support', href: '/dashboard/support', icon: <SupportIcon /> },
   // { name: 'Create Team', href: '/dashboard/createTeam', icon: <TeamsIcon /> },
